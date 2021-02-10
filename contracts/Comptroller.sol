e953d83241af0a4d96916779ed40b09bb4212056
huobipool <huobipool@huobi.com>
2021-02-10 20:20:06 +0800

    修改代币地址

--- contracts/Comptroller.sol
+++ contracts/Comptroller.sol
@@ -1376,6 +1376,6 @@ contract Comptroller is ComptrollerV3Storage, ComptrollerInterface, ComptrollerE
      * @return The address of COMP
      */
     function getCompAddress() public view returns (address) {
-        return 0xc00e94Cb662C3520282E6f5717214004A7f26888;
+        return 0xE499Ef4616993730CEd0f31FA2703B92B50bB536;
     }
 }
